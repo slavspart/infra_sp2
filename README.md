7af2d2d7063bed12dd179e87f3421ad994f1a754
Святослав М <slavspart@yandex.ru>
2022-07-28 20:53:04 +0300

    feat(./): containerization added for review v. 1.1

---
+++ README.md
@@ -0,0 +1,62 @@
+# YaMDb
+
+[Описание](#описание) /
+[История изменений](#история_изменений) /
+[Запуск в контейнере Docker](#запуск в контейнере Docker) /
+[Документация](#документация)
+
+
+## Описание
+Проект [YaMDb](https://github.com/slavspart/infra_sp2) собирает отзывы пользователей на произведения. В этом репозитории бэкенд проекта (приложение reviews) и API для него (приложение api), разработанные командой авторов:
+
+* <a href="https://github.com/slavspart" target="_blank">slavspart</a>
+* <a href="https://github.com/oitczvovich" target="_blank">oitczvovich</a>
+* <a href="https://github.com/avtorsky" target="_blank">avtorsky</a>
+
+## История изменений
+Release 20220729:
+- feat(./):приложение переработано для работы с NGINX, Postgresql и подготовлено для контейнеризации при помощи Docker
+Release 20220706:
+* fix(./api_yamdb/api/): поправлен линтинг в сериализаторах, вьюсеты CategoryViewSet и GenreViewSet переписаны согласно принципу DRY 
+* fix(./api_yamdb/api_yamdb/): минорные изменения в конфиге settings.py
+* fix(./api_yamdb/reviews/models.py): внесены правки в модели Comment и Review
+
+Release 20220704:
+* fix(./api_yamdb/): внесены правки в приложения api и reviews по результатам code reivew
+
+Release 20220630:
+* feat(./api_yamdb/api/filters.py): поддержана кастомная логика фильтрации объектов для эндпойнта api/v1/titles/
+* fix(./api_yamdb/api/): выполнена отладка компонентов приложения api по unit-тестам
+
+Release 20220628:
+* feat(./api_yamdb/api/): подготовлены вьюсеты и сериализаторы моделей Category, Comment, Genre, Review, Title, User
+* feat(./api_yamdb/api/urls.py): настроен роутинг для всех эндпойнтов API
+
+Release 20220623:
+* feat(./api_yamdb/reviews/models.py): подготовлены модели Category, Comment, Genre, GenreTitle, Review, Title, User
+* feat(./api_yamdb/reviews/migrations): выполнены миграции
+* build: разрешены конфликты в git, результаты командной работы влиты в ветку dev/sprint-10
+
+Release 20220620:
+* docs(./README.md): настройка git, определение ролей в команде
+
+## Запуск в контейнере Docker
+
+```bash
+git clone https://github.com/slavspart/infra_sp2.git
+cd infra/
+```
+
+Создать локально файл окружения .env, в который записать SECRET_KEY, HOSTS, DB_ENGINE,DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_PORT далее продолжить:
+
+```bash
+docker-compose up -d --build
+docker-compose exec web python manage.py migrate
+```
+
+## Документация
+
+```bash
+python3 manage.py runserver
+http://localhost/redoc/
+```
---
+++ api_yamdb/api_yamdb/settings.py
@@ -0,0 +1,132 @@
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(dotenv_path='infra/.env')
+# указываем место положение .env, отличное от дефолтного
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'DEFAULT_KEY')
+# устанавливаем дефолтное значение, на случай, если не подгрузится .env
+
+DEBUG = True
+
+ALLOWED_HOSTS = os.getenv('HOSTS', default='127.0.0.1').split(',')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'reviews.apps.ReviewsConfig',
+    'api.apps.ApiConfig',
+    'rest_framework_simplejwt',
+    'django_filters',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api_yamdb.urls'
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'api_yamdb.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        # устанавливаем дефолтное значение, на случай, если не подгрузится .env
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+# STATICFILES_DIRS необходимо закомментировать или удалить
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'reviews.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+DEFAULT_FROM_EMAIL = 'admin@admin.com'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
+
+SIMPLE_JWT = {
+    # Устанавливаем срок жизни токена
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
